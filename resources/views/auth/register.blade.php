174cf0e904121f639ce333d43244965f4888f978
uryngaliev <zhainar999@gmail.com>
2023-04-03 16:30:11 +0600

    change header for register view

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,66 +1,49 @@
-<!doctype html>
-<html>
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    @vite('resources/css/app.css')
-</head>
-<body>
-<header class="py-8 text-lg bg-[#111827]">
-    <div class="container mx-auto px-4">
-        <nav class="flex items-center justify-between">
-            <div>
-                <a class="text-white text-xl" href="{{route('home')}}">Logo</a>
+@extends('welcome')
+@section('title', 'Регистрация')
+@section('content')
+    <div class="bg-w-full max-w-xs mx-auto mt-8">
+        <form action="{{route('store')}}" method="post" class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+            @csrf
+            <div class="mb-4">
+                <label class="block text-gray-700 text-sm font-bold mb-2" for="name">
+                    Name
+                </label>
+                <input
+                    class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                    name="name" id="name" type="text" placeholder="Name">
             </div>
-            <div>
-                <a class="bg-blue-700 text-white py-2 px-6 rounded-xl" href="{{route('register')}}">Sign in</a>
+            <div class="mb-4">
+                <label class="block text-gray-700 text-sm font-bold mb-2" for="email">
+                    Email
+                </label>
+                <input
+                    class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                    name="email" id="email" type="email" placeholder="Email">
             </div>
-        </nav>
+            <div class="mb-4">
+                <label class="block text-gray-700 text-sm font-bold mb-2" for="password">
+                    Password
+                </label>
+                <input
+                    class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                    name="password" id="password" type="password" placeholder="Password">
+            </div>
+            <div class="mb-4">
+                <label class="block text-gray-700 text-sm font-bold mb-2" for="password_confirmation">
+                    Password Confirm
+                </label>
+                <input
+                    class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                    name="password_confirmation" id="password_confirmation" type="password"
+                    placeholder="Password Confirm">
+            </div>
+            <div class="flex items-center justify-between">
+                <button
+                    class="w-full bg-blue-700 hover:bg-blue-900 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                    type="submit">
+                    Sign In
+                </button>
+            </div>
+        </form>
     </div>
-</header>
-<div class="bg-w-full max-w-xs mx-auto mt-8">
-    <form action="{{route('store')}}" method="post" class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
-        @csrf
-        <div class="mb-4">
-            <label class="block text-gray-700 text-sm font-bold mb-2" for="name">
-                Name
-            </label>
-            <input
-                class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                name="name" id="name" type="text" placeholder="Name">
-        </div>
-        <div class="mb-4">
-            <label class="block text-gray-700 text-sm font-bold mb-2" for="email">
-                Email
-            </label>
-            <input
-                class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                name="email" id="email" type="email" placeholder="Email">
-        </div>
-        <div class="mb-4">
-            <label class="block text-gray-700 text-sm font-bold mb-2" for="password">
-                Password
-            </label>
-            <input
-                class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                name="password" id="password" type="password" placeholder="Password">
-        </div>
-        <div class="mb-4">
-            <label class="block text-gray-700 text-sm font-bold mb-2" for="password_confirmation">
-                Password Confirm
-            </label>
-            <input
-                class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                name="password_confirmation" id="password_confirmation" type="password" placeholder="Password Confirm">
-        </div>
-        <div class="flex items-center justify-between">
-            <button
-                class="w-full bg-blue-700 hover:bg-blue-900 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-                type="submit">
-                Sign In
-            </button>
-        </div>
-    </form>
-</div>
-</body>
-</html>
+@endsection
