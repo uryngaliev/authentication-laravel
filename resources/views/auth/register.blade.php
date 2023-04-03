6f57d802deab5f3ea4f23eacb92756a4bb0db463
uryngaliev <zhainar999@gmail.com>
2023-04-03 13:37:30 +0600

    change type button to submit

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -44,7 +44,7 @@
         <div class="flex items-center justify-between">
             <button
                 class="w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-                type="button">
+                type="submit">
                 Sign In
             </button>
         </div>
